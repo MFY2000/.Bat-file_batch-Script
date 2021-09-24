645064bee4b46f55d7a14d6300a3bfc5c662b6fb
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-09-24 22:05:32 +0500

    MFY 2021-09-24 22:05:32

--- Git Auto Commit -v2/Commiter/Status.py
+++ Git Auto Commit -v2/Commiter/Status.py
@@ -7,11 +7,37 @@ from os import path
 def main():
   Status()
 
+untrack = []
+modified = []
+
 def Status():
   repo = Repo(Address(), odbt=GitCmdObjectDB)
-  print(repo)
+
+  diff = repo.git.diff('HEAD~1..HEAD', name_only=True)
+
+  for i in repo.untracked_files:
+    untrack.append(i)
+
+  changed = [ item.a_path for item in repo.index.diff(None) ]
+
+  for item in repo.index.diff(None):
+    print(item.a_path)
+
+  for i in changed:
+      modified.append(i)
+      
+  print(untrack, modified)
+
+
+
+
+
 
 def Address():
-  url = r"C:\Users\MFY\Desktop\I-LOVE-GIT-COMMITS"
+  url = r"C:\Users\MFY\Desktop\.Bat-file_batch-Script"
   return url
-    os.system('cmd /c "cd C:\Users\MFY\Desktop\I-LOVE-GIT-COMMITS"')
+    # os.system('cmd /c "cd C:\Users\MFY\Desktop\I-LOVE-GIT-COMMITS"')
+
+
+if __name__ == '__main__':
+    main()
