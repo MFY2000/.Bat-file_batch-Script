5b2d0365f7da5af89fe852fbdde7e42c292070f5
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-09-24 23:05:05 +0500

    MFY auto commit at 24/09/2021 23:05:04

--- Git Auto Commit -v2/Commiter/Commit.py
+++ Git Auto Commit -v2/Commiter/Commit.py
@@ -9,35 +9,24 @@ from git.db import GitCmdObjectDB
 
 CurrentDate = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-def gitCommit(address):
-  if(isStatus(address) is not True):
-    return 0
-
+def gitCommit(address, message):
   repo = Repo(address, odbt = GitCmdObjectDB)
 
-  files = repo.git.diff(None, name_only=True)
   for f in Status(address):
-      repo.git.add(f)
+    repo.git.add(f)
+    repo.git.commit('-m', message)
 
-  repo.git.commit('-m', 'test commit')
-
-
-
-    # os.system('cmd /c "git add -A && git commit -m \"MFY auto commit on"' + CurrentDate + '\""')
 
 
 def push():
     os.system('cmd /c "git push -u origin"')
 
 
-def Address():
-    url = r"C:\Users\MFY\Desktop\I-LOVE-GIT-COMMITS"
-
 
 
 def main():
   # print(Status(r"C:\Users\MFY\Desktop\.Bat-file_batch-Script"))
-  print(gitCommit(r"C:\Users\MFY\Desktop\.Bat-file_batch-Script"))
+  print(gitCommit(r"C:\Users\MFY\Desktop\.Bat-file_batch-Script", "MFY auto commit at "+CurrentDate))
 
 
 
