0801116d6e3d9ee36b3b2b3c1e45dd0079b68356
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-09-26 14:41:26 +0500

    auto-commit

--- Git Auto Commit -v2/Commiter/Date.py
+++ Git Auto Commit -v2/Commiter/Date.py
@@ -1,25 +1,7 @@
-import sys
-from _datetime import datetime
+import ctypes, sys, win32api
+from datetime import datetime
 
-time_tuple = (2012,  # Year
-              9,  # Month
-              6,  # Day
-              0,  # Hour
-              38,  # Minute
-              0,  # Second
-              0,  # Millisecond
-              )
-
-
-def _win_set_time(time_tuple):
-    import win32api
-    dayOfWeek = datetime(*time_tuple).isocalendar()[2]
-    t = time_tuple[:2] + (dayOfWeek,) + time_tuple[2:]
-    win32api.SetSystemTime(*t)
-
-
-import win32api
-import ctypes, sys
+time_tuple = (2012,9,6,0,38,0,0)# Year, # Month, # Day, # Hour, # Minute, # Second, # Millisecond
 
 def is_admin():
     try:
@@ -29,7 +11,7 @@ def is_admin():
 
 def setDate():
     if is_admin():
-        win32api.SetSystemTime(2020,9,1,21,9,10,10,0)
+        win32api.SetSystemTime(2012,9,6,0,38,0,0)
     else:
         # Re-run the program with admin rights
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
@@ -41,7 +23,7 @@ def getDate():
 def main():
     CurrentDate = datetime.now().strftime("%Y,%m,%d,%H,%M,%S")
     # ShitDate = datetime.now().strftime("2020,9,1,21,9,10,10,0")
-    # setDate()
+    setDate()
     # input()
     # setDate(CurrentDate)
     print(CurrentDate)
