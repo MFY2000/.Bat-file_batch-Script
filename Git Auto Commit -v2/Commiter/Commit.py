f711de5b34d839f7b970ef812224a88b4efc0bd2
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-10-01 12:32:34 +0500

    MFY auto commit at 01/10/2021 12:32:34

--- Git Auto Commit -v2/Commiter/Commit.py
+++ Git Auto Commit -v2/Commiter/Commit.py
@@ -48,39 +48,53 @@ class Git:
 
 # 
   def gitChange_date(self,Start,End):
-    print(Start)
-
+  # Initailize Varrabile 
     CurrentDate = self.Date.getDate()
     NoOfDays_Differ = self.Date.getDaysDiffer(End,Start)
     countCommit = 0
+  # 
+    
 
-
+  #Used to define the number of commit per day 
     if self.status.changes == 0:
       return
-    elif NoOfDays_Differ >= len(self.status.changes):
+    
+    elif (len(self.status.changes) / NoOfDays_Differ) > 1:
+      countCommit = (int) (len(self.status.changes) / NoOfDays_Differ)
+    
+    elif (len(self.status.changes) / NoOfDays_Differ) < 1:
+      NoOfDays_Differ = len(self.status.changes)
       countCommit = 1
-    elif NoOfDays_Differ >= (len(self.status.changes) / 2):
-      countCommit = 2
+    
     else:
-      countCommit = len(self.status.changes) / NoOfDays_Differ
+      NoOfDays_Differ = 0
+      countCommit = 0
       # print("Error need to sort")
 
+  # 
+      # countCommit = 1
     # _myDate.
     for i in range(NoOfDays_Differ):
       self.Date.setDate(Start.day,Start.month,Start.year)
       Start = Start + timedelta(days=i)
-      print(Start, "Start move to")
       for j in range(countCommit):
+        # Git.gitCommit_Number(self,self.status.changes[0])
         self.message = "Auto commits Done: "+self.Date.getDate()
         print(self.Date.getDate(), "in working")
-        # Git.gitCommit_Number(self,self.status.changes[j])
-        del self.status.changes[j]
+        del self.status.changes[0]
+        # print(j,i, " adaad " , countCommit,NoOfDays_Differ)
+        
+    a = input()
+      
 
+      # print(Start, "Start move to", i)
+      #   input()
+    
     self.Date.setDate(CurrentDate.day,CurrentDate.month,CurrentDate.year)
-    # Git.gitPush(self)
     print(self.Date.getDate(), "After complete")
-
     input()
+    # Git.gitPush(self)
+    
 
   def run(self,_gateWay):
     if(_gateWay): 
@@ -97,7 +111,7 @@ def main():
     
     obj = Git(r"C:\Users\MFY\Desktop\.Bat-file_batch-Script",("MFY auto commit at "+_Date().getDate()))
     
-    Start = datetime(2021,9,28)
+    Start = datetime(2021,9,25)
     End = datetime(2021,9,30)
 
     # Feb 25, 2021 
@@ -114,7 +128,11 @@ def main():
 
   # obj.main(False)
 
+def run():
+  obj = Git(r"C:\Users\MFY\Desktop\.Bat-file_batch-Script",("MFY auto commit at "+_Date().getDate()))
+  obj.run(False)
+
 
 if __name__ == '__main__':
-  main()
+  run()
   
