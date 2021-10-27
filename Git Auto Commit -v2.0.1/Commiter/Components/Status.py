31ce1de84dc68ef9606ecbf64f9b8e45e3967461
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-10-27 07:04:25 +0500

    MFY auto commit at 27/10/2021 07:04:10

---
+++ Git Auto Commit -v2.0.1/Commiter/Components/Status.py
@@ -0,0 +1,17 @@
+class Status:
+  def __init__(self, repo):
+    self.repo = repo
+    self.changes = []
+    Status.status(self)
+
+  def status(self):
+
+    untrack = [ item for item in self.repo.untracked_files ]
+    modified = [ item.a_path for item in self.repo.index.diff(None) ]
+    
+    self.changes = untrack + modified
+
+  def isStatus(self):
+    result = self.repo.untracked_files != [] or self.repo.index.diff(None) != []
+    return (result)
+
