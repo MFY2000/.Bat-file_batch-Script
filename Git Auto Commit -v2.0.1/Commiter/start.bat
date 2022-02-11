2d8d4d86c4a1a45550f5f9bc7bdf9e1377da7c77
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-02-11 20:13:43 +0500

    MFY auto commit at 11/02/2022 20:13:35

--- Git Auto Commit -v2.0.1/Commiter/start.bat
+++ Git Auto Commit -v2.0.1/Commiter/start.bat
@@ -1,6 +1,6 @@
 @echo off
 
-python "C:\Users\MFY\Desktop\.Bat-file_batch-Script\Git Auto Commit -v2.0.1\Commiter\main.py"
+python "C:\Users\DarkJoker\Desktop\Git-Auto-Commit\Git Auto Commit -v2.0.1\Commiter\main.py"
 
 pause
 
