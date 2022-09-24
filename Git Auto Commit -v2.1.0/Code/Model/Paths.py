ee7ff5faf3da634bade49b0634a1d430b42694d0
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-09-24 19:35:28 +0500

    MFY auto commit at 24/09/2022 19:22:58

---
+++ Git Auto Commit -v2.1.0/Code/Model/Paths.py
@@ -0,0 +1,14 @@
+class ImagePath:
+    dashborad_background = r"../../Image/Backound/Dashborad_Background.png"
+    closeIcon = r"../../Image/Icon/Icon ionic-md-close.png"
+    minimizeIcon = r"../../Image/Icon/Icon awesome-minus.png"
+    nav_header = r"../../Image/Text/Group 13.png"
+    nav_dashborad = r"../../Image/Text/Group 11.png"
+    nav_path = r"../../Image/Text/Group 12.png"
+    nav_commitby = r"../../Image/Text/Group 14.png"
+    ButtonBackground = r"../../Image/Backound/Button_Bacgroung_G.png"
+    checkBoxBackground = r"../../Image/Button/CheckBox.png"
+    checkBoxIcon = r"../../Image/Icon/Icon_check_GR.png"
+
+    # _background = "../../Image/Backound/Dashborad_Background.png"
+    # dashborad_background = "../../Image/Backound/Dashborad_Background.png"
